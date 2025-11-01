e058e3830afa3e01680df684027adb79588868c9
vbhargavbtk <vbhargavbtk@gmail.com>
2025-11-01 19:07:19 +0000

    fix: Remove deprecated mongoose options

--- backend/src/server.js
+++ backend/src/server.js
@@ -26,7 +26,7 @@ app.use('/api/food', foodRoutes);
 app.use('/api/announcements', announcementRoutes);
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
