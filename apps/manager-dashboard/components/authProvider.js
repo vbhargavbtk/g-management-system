2828181f107cad0a7a6acbf0cbd1fdb2f97229b7
vbhargavbtk <vbhargavbtk@gmail.com>
2025-11-01 22:11:30 +0000

    Disable authentication for Manager Dashboard

--- apps/manager-dashboard/components/authProvider.js
+++ apps/manager-dashboard/components/authProvider.js
@@ -2,29 +2,10 @@ import { auth } from '../firebase';
 import { signInWithEmailAndPassword, signOut } from 'firebase/auth';
 
 const authProvider = {
-  login: ({ username, password }) => {
-    return signInWithEmailAndPassword(auth, username, password);
-  },
-  logout: () => signOut(auth),
-  checkAuth: () => {
-    return new Promise((resolve, reject) => {
-      auth.onAuthStateChanged(user => {
-        if (user) {
-          resolve();
-        } else {
-          reject();
-        }
-      });
-    });
-  },
-  checkError: (error) => {
-    const status = error.status;
-    if (status === 401 || status === 403) {
-      signOut(auth);
-      return Promise.reject();
-    }
-    return Promise.resolve();
-  },
+  login: () => Promise.resolve(), // Always resolve login
+  logout: () => Promise.resolve(), // Always resolve logout
+  checkAuth: () => Promise.resolve(), // Always resolve checkAuth
+  checkError: () => Promise.resolve(), // Always resolve checkError
   getPermissions: () => Promise.resolve(),
 };
 
