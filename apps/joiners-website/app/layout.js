8347f116f3fcddc9327dd6756c1787600a333523
vbhargavbtk <vbhargavbtk@gmail.com>
2025-11-02 03:40:39 +0000

    Fix Header and Footer import paths in joiners-website layout

--- apps/joiners-website/app/layout.js
+++ apps/joiners-website/app/layout.js
@@ -1,6 +1,6 @@
 import "./globals.css";
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 export const metadata = {
   title: "Create Next App",
